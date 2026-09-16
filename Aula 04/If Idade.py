5caee99ca75f16a73e8e7b34b2994cec2c174606
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-16 20:23:36 -0300

    Upload Arquivos Aula 04

---
+++ Aula 04/If Idade.py
@@ -0,0 +1,16 @@
+idade = int(input("Digite a sua idade: "))
+
+
+#Condicional composta (Se, Senão)
+#if (idade> 18):
+#    print("Parabéns, você é maior de idade kkkkkk")
+#else:
+#    print("Você é menor de idade")
+
+#Condicional Encadeada (Se, Senão Se, Senão)
+if (idade> 18):
+    print("Parabéns, você é maior de idade kkkkkk")
+elif(idade == 18):
+    print("Nossa, você tem exatamente 18 anos")
+else:
+    print("Você é menor de idade")
